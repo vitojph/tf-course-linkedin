a7d077295b70ed473a19d79915760ed8c6a90417
Victor Peinado <vitojph@gmail.com>
2017-08-04 18:27:17 +0200

    my first tf session

--- Exercise Files/02/addition.py
+++ Exercise Files/02/addition.py
@@ -3,17 +3,18 @@ import tensorflow as tf
 
 # Turn off TensorFlow warning messages in program output
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# 0: display all logs
+# 1: filter out INFO logs
+# 2: filter out WARNING logs
+# 3: filter out ERROR logs
 
 # Define computational graph
-X =
-Y =
+X = tf.placeholder(tf.float32, name='X')
+Y = tf.placeholder(tf.float32, name='X')
 
-addition =
-
+addition = tf.add(X, Y, name='addition')
 
 # Create the session
 with tf.Session() as session:
-
-    result =
-
+    result = session.run(addition, feed_dict={X: [3, -23, 877, 14303], Y: [4, 0.00234, 1000, 21]})
     print(result)
